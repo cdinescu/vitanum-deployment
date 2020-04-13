afea4d31ec47f3cfc87c788b5b8929e4aad0301b
crcostea <crina91@gmail.com>
2020-04-13 22:12:26 +0300

    Created integration test for: food-service: Part3

--- tests/test_food_service.sh
+++ tests/test_food_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SEARCH_URL='http://127.0.0.1:8082/foods/search?foodSearchKeyword='
-SEARCH_TOKEN='tomato'
+REPORTS_URL='http://127.0.0.1:8082/foods/reports/'
 
 log() {
   echo -e "\n======================================================\n"
@@ -9,6 +9,8 @@ log() {
 
 test_FoodFound() {
   log
+
+  SEARCH_TOKEN='tomato'
   URL="${SEARCH_URL}${SEARCH_TOKEN}"
   HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
 
@@ -38,6 +40,7 @@ test_FoodFound() {
 
 test_FoodNotFound() {
   log
+
   ITEM_NOT_FOUND="randomestrandomthing"
   URL="${SEARCH_URL}${ITEM_NOT_FOUND}"
   HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
@@ -57,6 +60,63 @@ test_FoodNotFound() {
   fi
 }
 
+test_FoodReport_ValidFoodId() {
+  log
+
+  FOOD_ID="783051"
+  URL="${REPORTS_URL}${FOOD_ID}"
+  HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
+
+  # extract the body
+  HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+  # extract the status
+  HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+
+  # example using the status
+  if [ ! $HTTP_STATUS -eq 200  ]; then
+    echo "Failed: [HTTP status: $HTTP_STATUS]"
+    exit 1
+  else
+    echo "Success: HTTP_STATUS is 200"
+  fi
+
+  if [[ -z "$HTTP_BODY" ]]
+  then
+    echo "Failed: HTTP body is empty"
+    exit 1
+  else
+    echo -e "Success: HTTP body is not empty:\n ${HTTP_BODY}"
+  fi
+}
+
+test_FoodReport_InvalidFoodId() {
+  log
+
+  FOOD_ID="bla"
+  URL="${REPORTS_URL}${FOOD_ID}"
+
+  HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
+
+  # extract the body
+  HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+  # extract the status
+  HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+  # example using the status
+  if [ ! $HTTP_STATUS -eq 404  ]; then
+    echo "Failed: [HTTP status: $HTTP_STATUS]"
+    exit 1
+  else
+    echo "Success: HTTP_STATUS is 404"
+  fi
+}
+
+
 # Run the tests
 test_FoodFound
 test_FoodNotFound
+test_FoodReport_ValidFoodId
+test_FoodReport_InvalidFoodId
