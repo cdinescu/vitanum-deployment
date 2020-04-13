b34252763f1c4909bb6ca777e66775232c9e2422
crcostea <crina91@gmail.com>
2020-04-13 21:50:43 +0300

    Created integration test for: food-service: Part2

--- tests/test_food_service.sh
+++ tests/test_food_service.sh
@@ -2,9 +2,13 @@
 
 SEARCH_URL='http://127.0.0.1:8082/foods/search?foodSearchKeyword='
 SEARCH_TOKEN='tomato'
-HTTP_STATUS_OK=200
 
-test_FoodFound_NonEmptyBody() {
+log() {
+  echo -e "\n======================================================\n"
+}
+
+test_FoodFound() {
+  log
   URL="${SEARCH_URL}${SEARCH_TOKEN}"
   HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
 
@@ -32,4 +36,27 @@ test_FoodFound_NonEmptyBody() {
   fi
 }
 
-test_FoodFound_NonEmptyBody
+test_FoodNotFound() {
+  log
+  ITEM_NOT_FOUND="randomestrandomthing"
+  URL="${SEARCH_URL}${ITEM_NOT_FOUND}"
+  HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
+
+  # extract the body
+  HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+  # extract the status
+  HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+  # example using the status
+  if [ ! $HTTP_STATUS -eq 404  ]; then
+    echo "Failed: [HTTP status: $HTTP_STATUS]"
+    exit 1
+  else
+    echo "Success: HTTP_STATUS is 404"
+  fi
+}
+
+# Run the tests
+test_FoodFound
+test_FoodNotFound
